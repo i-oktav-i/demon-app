9de28c3ea149de825e2088ccabc2bc1d5d22662b
a.taranenko <i_oktav_i@mail.ru>
2020-12-07 19:49:41 +0300

    Улучшена форма

--- src/views/Form.vue
+++ src/views/Form.vue
@@ -1,19 +1,36 @@
 <template>
-  <div class="form">
-    <el-input
-      placeholder="Площадь участка (в квадратных метрах)"
-      v-model="input1"
-    />
-    <el-input
-      placeholder="Количество парковочных мест"
-      v-model="input2"
-    />
-    <el-input
-      placeholder="Тип домика"
-      v-model="input3"
-    />
+  <div class="formPage">
+    <div class="form">
+      <el-input
+        placeholder="Площадь участка (в квадратных метрах)"
+        v-model="input1"
+      />
+      <el-input
+        placeholder="Количество парковочных мест"
+        v-model="input2"
+      />
+      <el-input
+        placeholder="Тип домика"
+        v-model="input3"
+      />
 
-    <p>{{ result }}</p>
+      <el-select v-model="selec1Value" placeholder="Тип хуя">
+        <el-option
+          v-for="item in select1"
+          :key="item"
+          :label="item"
+          :value="item">
+        </el-option>
+      </el-select>
+      <el-button
+        v-loading="isLoading"
+        @click="onClick"
+      >
+        ПИПИСЬКА
+      </el-button>
+    </div>
+
+    <div class="img" :style="{backgroundImage: `url(${imgUrl})`}" />
   </div>
 </template>
 
@@ -30,14 +47,51 @@ export default class Menu extends Vue {
 
   input3: number | null = null
 
-  get result() {
-    return (this.input1 || 0) * (this.input2 || 0) + (this.input3 || 0);
+  select1 = [
+    'хуй',
+    'все же хуй?',
+    'Или хуй',
+  ]
+
+  select1Value = this.select1[0]
+
+  isLoading: boolean | null = null
+
+  async onClick() {
+    this.isLoading = true;
+
+    await new Promise<void>((resolve) => setTimeout(() => resolve(), 1000));
+
+    this.isLoading = false;
+  }
+
+  images = [
+    'https://yt3.ggpht.com/a/AATXAJx_ORV9RJbkswXpGTgkpFM3yGGrOg6MNT3-9A=s900-c-k-c0xffffffff-no-rj-mo',
+  ]
+
+  get imgUrl() {
+    if (this.isLoading || this.isLoading === null) return '';
+
+    return this.images[Math.floor(Math.random() * this.images.length)];
   }
 }
 </script>
 
 <style lang="scss" scoped>
+.formPage {
+  display: flex;
+}
+
 .form {
   max-width: 600px;
 }
+
+.img {
+  width: 100%;
+  height: 50vh;
+  max-width: 300px;
+  max-height: 200px;
+  background: no-repeat center / contain;
+}
+
 </style>
